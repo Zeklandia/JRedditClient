fa5f875c82fa914b68067e81fe9861ae888f8f08
Asher Wood <awood5659@gmail.com>
2014-03-05 16:36:55 -0500

    Initial Commit

---
+++ src/zeklandia/desktop/JavaReddit.java
@@ -0,0 +1,20 @@
+package zeklandia.desktop;
+
+/**
+ *
+ * @author Zeklandia
+ */
+
+import zeklandia.desktop.reddit.client.ClientMain;
+
+public class JavaReddit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        ClientMain.main();
+    }
+    
+}
