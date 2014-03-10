bf16f46ab8ce87e9dc3fa36e94b7df2d429ce5ba
Asher Wood <awood5659@gmail.com>
2014-03-10 16:46:54 -0400

    Rename, work on LogOutput

--- src/zeklandia/desktop/reddit/RedditMain.java
+++ src/zeklandia/desktop/reddit/RedditMain.java
@@ -6,18 +6,18 @@ import zeklandia.desktop.reddit.client.RedditClientMain;
 
 public class RedditMain {
 
-    private static final String classname = "RedditMain";
-    private static final String padding = "       ";
-    public static final String banner = "                   [0;1;36;96m#[0m      [0;1;33;93m#[0m    [0;1;36;96m\"[0m      [0;1;33;93mm[0m   \n"
-	    + "  [0;1;32;92mm[0m [0;1;36;96mmm[0m   [0;1;35;95mm[0;1;31;91mmm[0m    [0;1;36;96mmm[0;1;34;94mm#[0m   [0;1;31;91mm[0;1;33;93mmm[0;1;32;92m#[0m  [0;1;36;96mm[0;1;34;94mmm[0m    [0;1;33;93mmm[0;1;32;92m#m[0;1;36;96mm[0m \n"
-	    + "  [0;1;36;96m#\"[0m  [0;1;35;95m\"[0m [0;1;31;91m#\"[0m  [0;1;32;92m#[0m  [0;1;36;96m#[0;1;34;94m\"[0m [0;1;35;95m\"#[0m  [0;1;33;93m#\"[0m [0;1;32;92m\"[0;1;36;96m#[0m    [0;1;35;95m#[0m      [0;1;36;96m#[0m   \n"
-	    + "  [0;1;34;94m#[0m     [0;1;33;93m#\"[0;1;32;92m\"\"[0;1;36;96m\"[0m  [0;1;34;94m#[0m   [0;1;31;91m#[0m  [0;1;32;92m#[0m   [0;1;34;94m#[0m    [0;1;31;91m#[0m      [0;1;34;94m#[0m   \n"
-	    + "  [0;1;35;95m#[0m     [0;1;32;92m\"#[0;1;36;96mmm[0;1;34;94m\"[0m  [0;1;35;95m\"[0;1;31;91m#m[0;1;33;93m##[0m  [0;1;36;96m\"#[0;1;34;94mm#[0;1;35;95m#[0m  [0;1;31;91mm[0;1;33;93mm#[0;1;32;92mmm[0m    [0;1;35;95m\"m[0;1;31;91mm[0m ";
+	private static final String classname = "RedditMain";
+	private static final String padding = "       ";
+	public static final String banner = "                   [0;1;36;96m#[0m      [0;1;33;93m#[0m    [0;1;36;96m\"[0m      [0;1;33;93mm[0m   \n"
+		+ "  [0;1;32;92mm[0m [0;1;36;96mmm[0m   [0;1;35;95mm[0;1;31;91mmm[0m    [0;1;36;96mmm[0;1;34;94mm#[0m   [0;1;31;91mm[0;1;33;93mmm[0;1;32;92m#[0m  [0;1;36;96mm[0;1;34;94mmm[0m    [0;1;33;93mmm[0;1;32;92m#m[0;1;36;96mm[0m \n"
+		+ "  [0;1;36;96m#\"[0m  [0;1;35;95m\"[0m [0;1;31;91m#\"[0m  [0;1;32;92m#[0m  [0;1;36;96m#[0;1;34;94m\"[0m [0;1;35;95m\"#[0m  [0;1;33;93m#\"[0m [0;1;32;92m\"[0;1;36;96m#[0m    [0;1;35;95m#[0m      [0;1;36;96m#[0m   \n"
+		+ "  [0;1;34;94m#[0m     [0;1;33;93m#\"[0;1;32;92m\"\"[0;1;36;96m\"[0m  [0;1;34;94m#[0m   [0;1;31;91m#[0m  [0;1;32;92m#[0m   [0;1;34;94m#[0m    [0;1;31;91m#[0m      [0;1;34;94m#[0m   \n"
+		+ "  [0;1;35;95m#[0m     [0;1;32;92m\"#[0;1;36;96mmm[0;1;34;94m\"[0m  [0;1;35;95m\"[0;1;31;91m#m[0;1;33;93m##[0m  [0;1;36;96m\"#[0;1;34;94mm#[0;1;35;95m#[0m  [0;1;31;91mm[0;1;33;93mm#[0;1;32;92mmm[0m    [0;1;35;95m\"m[0;1;31;91mm[0m ";
 
-    public static void main(String[] args) {
-	LogOutput.prepareConsole();
-	LogOutput.logMessage(padding, classname, "Program Started");
-	AnsiConsole.system_out.println(banner);
-	RedditClientMain.main();
-    }
+	public static void main(String[] args) {
+		LogOutput.prepareConsole();
+		LogOutput.logMessage(padding, classname, "Program Started");
+		AnsiConsole.system_out.println(banner);
+		RedditClientMain.main();
+	}
 }
